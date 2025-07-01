81becf7c7af008e18a3fdeeb04c2bc421a3c5a9f
Eureka <narike@gmail.com>
2025-07-01 16:15:41 +0300

    Add project files.

---
+++ Persistence/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Domain;
+using System.Diagnostics;
+
+namespace Persistence
+{
+    public class AppDbContext(DbContextOptions options) : DbContext(options)
+    {
+        public required DbSet<Domain.Activity> Activities { get; set; }
+    }
+}

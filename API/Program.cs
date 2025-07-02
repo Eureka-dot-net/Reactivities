71e50aaa2a59662dd7ce431fd0826051e5a9ae94
Eureka <narike@gmail.com>
2025-07-02 12:58:30 +0300

    End of section 3

--- API/Program.cs
+++ API/Program.cs
@@ -10,9 +10,16 @@ builder.Services.AddControllers();
 builder.Services.AddDbContext<AppDbContext>(opt =>
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
 
+builder.Services.AddCors();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+
+app.UseCors(x => x
+    .AllowAnyMethod()
+    .AllowAnyHeader()
+    .WithOrigins("http://localhost:3000", "https://localhost:3000")); 
 
 app.MapControllers();
 
